a2b6a06c86505d024dcaf9b4d270f5cf5d486cb8
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-09-28 12:41:08 -0600

    phase 1

---
+++ Wordle.py
@@ -0,0 +1,33 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+
+def wordle():
+
+    def enter_action(s):
+        gw.show_message(correct_answer_list)
+
+    correct_answer = random.choice(FIVE_LETTER_WORDS)
+    correct_answer_list = list(correct_answer)
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+
+    
+    for i, letter in  enumerate(correct_answer_list) :
+        gw.set_square_letter(0, i, letter) 
+    
+# Startup code
+
+if __name__ == "__main__":
+   wordle()
+
+
