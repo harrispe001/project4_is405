c935c56455ea8247fee1135e729493cebaed2425
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-09-28 12:46:18 -0600

    made output a function

--- Wordle.py
+++ Wordle.py
@@ -15,15 +15,18 @@ def wordle():
     def enter_action(s):
         gw.show_message(correct_answer_list)
 
+
+    def add_row() :
+        for i, letter in  enumerate(correct_answer_list) :
+            gw.set_square_letter(0, i, letter) 
+
     correct_answer = random.choice(FIVE_LETTER_WORDS)
     correct_answer_list = list(correct_answer)
 
     gw = WordleGWindow()
+    add_row()
     gw.add_enter_listener(enter_action)
 
-    
-    for i, letter in  enumerate(correct_answer_list) :
-        gw.set_square_letter(0, i, letter) 
     
 # Startup code
 
