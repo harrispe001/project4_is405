8ac5d43c8932f2a0f806e2f49dad05f507f62dd6
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-10-03 13:58:18 -0600

    yellow and green

--- Wordle.py
+++ Wordle.py
@@ -19,12 +19,39 @@ def wordle():
         if input_string.lower() == correct_answer.lower() :
             for col in range(0, 5):
                 gw.set_square_color(gw.get_current_row(), col, "#66BB66") 
-            gw.show_message(f'You Won! It took you {gw.get_current_row() + 1} trie(s)')
 
-        #what to do if enter valid word
+            #Output winning message
+            if gw.get_current_row() > 0 :
+                gw.show_message(f'You Won! It took you {gw.get_current_row() + 1} tries')
+            else :
+                gw.show_message(f'You Won! It took you {gw.get_current_row() + 1} try')
+
+        #what to do if enter valid word that is not correct
         elif input_string.lower() in FIVE_LETTER_WORDS:
             gw.show_message("That is a word!")
-            gw.set_current_row(gw.get_current_row() + 1)
+            
+            #finds greens and possible yellow
+            green_positions, yellow_cands = check_for_green(input_string, correct_answer)
+
+            #finds yellows
+            yellow_positions = check_for_yellow(input_string, correct_answer, yellow_cands)
+
+            #set tiles to their proper color
+            for pos in range(0, 5):
+                if pos in green_positions:
+                    gw.set_square_color(gw.get_current_row(), pos, "#66BB66")
+
+                #make tile yellow  - unless key is already green
+                elif pos in yellow_positions:
+                    gw.set_square_color(gw.get_current_row(), pos, "#CCBB66")
+
+                    if gw.get_key_color(input_string[pos]) != "#66BB66":
+                        gw.set_key_color(input_string[pos], "#CCBB66")
+
+            if gw.get_current_row()==5:
+                gw.show_message(f'You did not guess it! The correct answer is: {correct_answer.upper()}' )
+            else:
+                gw.set_current_row(gw.get_current_row() + 1)
 
         #What to do with invalid word
         else:
@@ -40,20 +67,49 @@ def wordle():
     def set_answer() :
         return random.choice(FIVE_LETTER_WORDS)
 
+    #Returns a list of green tiles and potential yellows
+    def check_for_green(guess, answer) :
+        green_pos = []
+        yellow_candidates = []
 
+        for n in range(0, 5):
+            if guess.lower()[n] == answer[n]:
+                green_pos.append(n)
+
+            else:
+                yellow_candidates.append(n)
+
+        return green_pos, yellow_candidates
 
     # get random word from word list and make it an array
 
+        #recieves the potentail yellows from the check_for_greens function and returns a list of yellows and by elimination grays
+    def check_for_yellow(guess, answer, yellow_cands):
+        yellow_pos = []
+        answer_string_not_green_chars = []
+        input_string_not_green_chars = []
 
-    correct_answer = set_answer()
-    #correct_answer_list = list(correct_answer)
-    correct_guess = False
+        for n in yellow_cands:
+            answer_string_not_green_chars.append(answer[n])
+            input_string_not_green_chars.append(guess.lower()[n])
+
+        for i in yellow_cands:
+            if guess.lower()[i] in answer_string_not_green_chars:
+                yellow_pos.append(i)
+                answer_string_not_green_chars.remove(guess.lower()[i])
+        
+        return yellow_pos
 
 
     
+    #Set correct answer for wordle game
+    #correct_answer = set_answer()
+    correct_answer = "happy"
+
+    #Start window    
     gw = WordleGWindow()
     
-        #temporary
+    #temporary show the answer for testing
     gw.show_message(correct_answer)
 
     gw.add_enter_listener(enter_action)
